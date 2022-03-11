294678318c2270bf34ea4f8723abf27e75397831
Priyanka Jain <jain198@purdue.edu>
2022-03-11 14:56:03 -0500

    created routes

--- src/App.js
+++ src/App.js
@@ -7,20 +7,34 @@ import Education from "./components/education/Education";
 import Experience from "./components/experience/Experience";
 import Skills from "./components/skills/Skills";
 import './app.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import ScrollToTop from "./ScrollToTop";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
-      <div className="sections">
-        <InteractiveGrid />
-        <Intro />
-        <Education />
-        <Experience />
-        <Skills />
-        <Portfolio />
-        <Contact />
-      </div>
+      <Router>
+        <Navbar />
+        <div className="sections">
+          <ScrollToTop>
+            <Routes>
+              <React.Fragment>
+                <Route path='/' element={<InteractiveGrid />} />
+                <Route path='/intro' element={<Intro />} />
+                <Route path='/education' element={<Education />} />
+                <Route path='/experience' element={<Experience />} />
+                <Route path='/skills' element={<Skills />} />
+                <Route path='/portfolio' element={<Portfolio />} />
+                <Route path='/Contact' element={<Contact />} />
+                <Route path="*" element={<Navigate replace to="/home" />} />
+              </React.Fragment>
+            </Routes>
+          </ScrollToTop>
+        </div>
+      </Router>
+
     </div>
   );
 }
