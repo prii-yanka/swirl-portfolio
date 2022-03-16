425303d6e36398075dd50b4d26cc0e6a421bfea1
Priyanka Jain <jain198@purdue.edu>
2022-03-16 00:16:10 -0500

    added react-three/fiber and animated dragon

--- src/pages/interactive-grid/InteractiveGrid.jsx
+++ src/pages/interactive-grid/InteractiveGrid.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import './interactive-grid.css'
 import '../pages.css'
 import { useNav } from '../../customHooks/useNav';
-import ScriptTag from 'react-script-tag';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
+import Box from '../../components/Box'
+import Dragon from '../../components/Dragon'
 
 const InteractiveGrid = () => {
 
   const interactiveGridRef = useNav('InteractiveGrid')
 
   return (
-    <section className='interactive-grid' ref={interactiveGridRef} id='interactiveGridContainer'>
-      <div>Interactive Grid</div>
-      <ScriptTag type="text/javascript" src="../../public/sketch.js" />
+    <section className="interactive-grid" ref={interactiveGridRef} id='interactiveGridContainer'>
+      <Canvas className="canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight intensity={0.5} position={[-2, 5, 2]} />
+        <Suspense fallback={null}>
+          <Dragon />
+        </Suspense>
+      </Canvas>
     </section>
   )
 }
