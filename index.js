8b2811a68e2568b670cedb382293abc4022865b8
prii-yanka <jain.prii.yanka@gmail.com>
2022-10-02 20:33:54 -0400

    changed server to index.js and created endpoints for different tags

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5001;
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(require("./routes/project"));
 // get driver connection
 const dbo = require("./db/conn");
+
+// app.get('/', (req,res) => {
+//   res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
+// });
  
 app.listen(port, () => {
   // perform a database connection when server starts
--- routes/project.js
+++ routes/project.js
@@ -11,30 +11,76 @@ const dbo = require("../db/conn");
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
  
+projectRoutes.route("/").get(function (req, res) {
+  let db_connect = dbo.getDb("portfolio");
+  db_connect
+    .collection("projects")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+ });
  
 // This section will help you get a list of all the records.
-projectRoutes.route("/projects").get(function (req, res) {
+projectRoutes.route("/featured").get(function (req, res) {
  let db_connect = dbo.getDb("portfolio");
  db_connect
    .collection("projects")
-   .find({})
+   .find({tags: "Featured"})
    .toArray(function (err, result) {
      if (err) throw err;
      res.json(result);
    });
 });
+
+// This section will help you get a list of all the records.
+projectRoutes.route("/web").get(function (req, res) {
+  let db_connect = dbo.getDb("portfolio");
+  db_connect
+    .collection("projects")
+    .find({tags: "Web App"})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+ });
+
+ // This section will help you get a list of all the records.
+projectRoutes.route("/design").get(function (req, res) {
+  let db_connect = dbo.getDb("portfolio");
+  db_connect
+    .collection("projects")
+    .find({tags: "Design"})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+ });
+
+ // This section will help you get a list of all the records.
+projectRoutes.route("/mobile").get(function (req, res) {
+  let db_connect = dbo.getDb("portfolio");
+  db_connect
+    .collection("projects")
+    .find({tags: "Mobile App"})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+ });
  
-// This section will help you get a single record by id
-projectRoutes.route("/projects/:id").get(function (req, res) {
- let db_connect = dbo.getDb();
- let myquery = { _id: ObjectId(req.params.id) };
- db_connect
-   .collection("projects")
-   .findOne(myquery, function (err, result) {
-     if (err) throw err;
-     res.json(result);
-   });
-});
+// // This section will help you get a single record by id
+// projectRoutes.route("/projects/:id").get(function (req, res) {
+//  let db_connect = dbo.getDb();
+//  let myquery = { _id: ObjectId(req.params.id) };
+//  db_connect
+//    .collection("projects")
+//    .findOne(myquery, function (err, result) {
+//      if (err) throw err;
+//      res.json(result);
+//    });
+// });
  
 // // This section will help you create a new record.
 // recordRoutes.route("/record/add").post(function (req, response) {
