b37259a97831080705c1a1c32b5ee22afeb43e1b
prii-yanka <jain.prii.yanka@gmail.com>
2022-10-18 01:52:22 -0400

    mongoose + nav up/down + amazon s3

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 const express = require("express");
-const path = require('path');
-const app = express();
+// const bodyParser = require("body-parser");
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5001;
+const path = require('path');
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
+
+const app = express();
+console.log(process.env);
+
+const Project = require("./models/project");
+
 app.use(cors({
   origin: '*'
 }));
 app.use(express.json());
 app.use(require("./routes/project"));
 // get driver connection
-const dbo = require("./db/conn");
+// const dbo = require("./db/conn");
+
 
 // app.get('/', (req,res) => {
 //   res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
 // });
- 
+const port = process.env.PORT || 5001;
 app.listen(port, () => {
   // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
+  // dbo.connectToServer(function (err) {
+  //   if (err) console.error(err);
  
-  });
+  // });
   console.log(`Server is running on port: ${port}`);
 });
+
+// module.exports = app;
--- routes/project.js
+++ routes/project.js
@@ -1,130 +1,134 @@
 const express = require("express");
- 
+
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.S3_ACCESS_KEY,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: process.env.S3_BUCKET_REGION,
+});
+
+// function getFileStream(fileKey) {
+//   const downloadParams = {
+//   Key: fileKey,
+//   Bucket: process.env.S3_BUCKET_NAME,
+// };
+// return s3.getObject(downloadParams).createReadStream();
+// }
+
 // recordRoutes is an instance of the express router.
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /record.
-const projectRoutes = express.Router();
- 
+const projectRouter = express.Router();
+
 // This will help us connect to the database
-const dbo = require("../db/conn");
- 
+// const dbo = require("../db/conn");
+const Project = require("../models/project");
+// const { request, response } = require("express");
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
- 
-projectRoutes.route("/").get(function (req, res) {
-  let db_connect = dbo.getDb("portfolio");
-  db_connect
-    .collection("projects")
-    .find({})
-    .toArray(function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
- });
- 
-// This section will help you get a list of all the records.
-projectRoutes.route("/featured").get(function (req, res) {
- let db_connect = dbo.getDb("portfolio");
- db_connect
-   .collection("projects")
-   .find({tags: "Featured"})
-   .toArray(function (err, result) {
-     if (err) throw err;
-     res.json(result);
-   });
+
+
+projectRouter.get("/", async (request, response) => {
+  Project.find({}).then((projects) => {
+    response.json(projects);
+  });
 });
 
-// This section will help you get a list of all the records.
-projectRoutes.route("/web").get(function (req, res) {
-  let db_connect = dbo.getDb("portfolio");
-  db_connect
-    .collection("projects")
-    .find({tags: "Web App"})
-    .toArray(function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
- });
+projectRouter.get("/image", async (request, response) => {
+  var params = { Bucket: process.env.S3_BUCKET_NAME, Key: "Add Recipe.png" };
+  s3.getObject(params, function (err, data) {
+    response.writeHead(200, { "Content-Type": "image/png" });
+    // response.write(data.Body);
+    // response.end();
+    response.write(data.Body, "binary");
+    response.end(null, "binary");
+  });
+});
 
- // This section will help you get a list of all the records.
-projectRoutes.route("/design").get(function (req, res) {
-  let db_connect = dbo.getDb("portfolio");
-  db_connect
-    .collection("projects")
-    .find({tags: "Design"})
-    .toArray(function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
- });
+projectRouter.get("/featured", async (request, response) => {
+  Project.find({ tags: "Featured" }).then((projects) => {
+    response.json(projects);
+  });
+});
 
- // This section will help you get a list of all the records.
-projectRoutes.route("/mobile").get(function (req, res) {
-  let db_connect = dbo.getDb("portfolio");
-  db_connect
-    .collection("projects")
-    .find({tags: "Mobile App"})
-    .toArray(function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
- });
- 
-// // This section will help you get a single record by id
-// projectRoutes.route("/projects/:id").get(function (req, res) {
-//  let db_connect = dbo.getDb();
-//  let myquery = { _id: ObjectId(req.params.id) };
+projectRouter.get("/web", async (request, response) => {
+  Project.find({ tags: "Web App" }).then((projects) => {
+    response.json(projects);
+  });
+});
+
+projectRouter.get("/design", async (request, response) => {
+  Project.find({ tags: "Design" }).then((projects) => {
+    response.json(projects);
+  });
+});
+
+projectRouter.get("/mobile", async (request, response) => {
+  Project.find({ tags: "Mobile App" }).then((projects) => {
+    response.json(projects);
+  });
+});
+
+// projectRoutes.route("/").get(function (req, res) {
+//   let db_connect = dbo.getDb("portfolio");
+//   db_connect
+//     .collection("projects")
+//     .find({})
+//     .toArray(function (err, result) {
+//       if (err) throw err;
+//       res.json(result);
+//     });
+//  });
+
+// // This section will help you get a list of all the records.
+// projectRoutes.route("/featured").get(function (req, res) {
+//  let db_connect = dbo.getDb("portfolio");
 //  db_connect
 //    .collection("projects")
-//    .findOne(myquery, function (err, result) {
+//    .find({tags: "Featured"})
+//    .toArray(function (err, result) {
 //      if (err) throw err;
 //      res.json(result);
 //    });
 // });
- 
-// // This section will help you create a new record.
-// recordRoutes.route("/record/add").post(function (req, response) {
-//  let db_connect = dbo.getDb();
-//  let myobj = {
-//    name: req.body.name,
-//    position: req.body.position,
-//    level: req.body.level,
-//  };
-//  db_connect.collection("records").insertOne(myobj, function (err, res) {
-//    if (err) throw err;
-//    response.json(res);
+
+// // This section will help you get a list of all the records.
+// projectRoutes.route("/web").get(function (req, res) {
+//   let db_connect = dbo.getDb("portfolio");
+//   db_connect
+//     .collection("projects")
+//     .find({tags: "Web App"})
+//     .toArray(function (err, result) {
+//       if (err) throw err;
+//       res.json(result);
+//     });
 //  });
-// });
- 
-// // This section will help you update a record by id.
-// recordRoutes.route("/update/:id").post(function (req, response) {
-//  let db_connect = dbo.getDb();
-//  let myquery = { _id: ObjectId(req.params.id) };
-//  let newvalues = {
-//    $set: {
-//      name: req.body.name,
-//      position: req.body.position,
-//      level: req.body.level,
-//    },
-//  };
-//  db_connect
-//    .collection("records")
-//    .updateOne(myquery, newvalues, function (err, res) {
-//      if (err) throw err;
-//      console.log("1 document updated");
-//      response.json(res);
-//    });
-// });
- 
-// // This section will help you delete a record
-// recordRoutes.route("/:id").delete((req, response) => {
-//  let db_connect = dbo.getDb();
-//  let myquery = { _id: ObjectId(req.params.id) };
-//  db_connect.collection("records").deleteOne(myquery, function (err, obj) {
-//    if (err) throw err;
-//    console.log("1 document deleted");
-//    response.json(obj);
+
+//  // This section will help you get a list of all the records.
+// projectRoutes.route("/design").get(function (req, res) {
+//   let db_connect = dbo.getDb("portfolio");
+//   db_connect
+//     .collection("projects")
+//     .find({tags: "Design"})
+//     .toArray(function (err, result) {
+//       if (err) throw err;
+//       res.json(result);
+//     });
 //  });
-// });
- 
-module.exports = projectRoutes;
+
+//  // This section will help you get a list of all the records.
+// projectRoutes.route("/mobile").get(function (req, res) {
+//   let db_connect = dbo.getDb("portfolio");
+//   db_connect
+//     .collection("projects")
+//     .find({tags: "Mobile App"})
+//     .toArray(function (err, result) {
+//       if (err) throw err;
+//       res.json(result);
+//     });
+//  });
+
+module.exports = projectRouter;
