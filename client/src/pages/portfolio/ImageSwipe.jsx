a7a00f398beff8946d824bcd94156bbc7f79881e
Priyanka Jain <jain.prii.yanka@gmail.com>
2023-06-21 00:33:34 -0500

    responsve carousel and thumbnails

--- client/src/pages/portfolio/ImageSwipe.jsx
+++ client/src/pages/portfolio/ImageSwipe.jsx
@@ -2,46 +2,32 @@ import React, { useEffect, useRef, useState } from "react";
 // import function to register Swiper custom elements
 import LeftArrow from "../../components/arrows/LeftArrow";
 import RightArrow from "../../components/arrows/RightArrow";
-import {
-  motion,
-  useScroll,
-  useTransform,
-} from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const ImageSwipe = ({ images, onLoad, imageStyle, imageDescriptions }) => {
   const sliderRef = useRef(null);
+  const [currImage, setCurrImage] = useState();
+  const [currImgContStyle, setCurrImgContStyle] = useState({ width: "45rem" });
   const [sliderData, setSliderData] = useState(0);
-  // const scrollXRef = useRef(null);
-
-  const [currentPrecent, setCurrentPercent] = useState(null)
+  const [currentPrecent, setCurrentPercent] = useState(null);
   const { scrollXProgress } = useScroll({
-    container: sliderRef
+    container: sliderRef,
   });
-  const xRange = useTransform(scrollXProgress, [0, 1], [0, 100], { clamp: false });
+  const xRange = useTransform(scrollXProgress, [0, 1], [0, 100], {
+    clamp: false,
+  });
+  const matches = useMediaQuery('(max-aspect-ratio : 3/4)');
+
 
   useEffect(
     () =>
-        xRange.onChange((v) => {
-            console.log(Math.trunc(xRange.current))
-            setCurrentPercent(Math.trunc(xRange.current))
-        }),
+      xRange.onChange((v) => {
+        // console.log(Math.trunc(xRange.current));
+        setCurrentPercent(Math.trunc(xRange.current));
+      }),
     [xRange]
-);
-
-// useEffect(() => {
-//   console.log(scrollXProgress);
-// }, [scrollXProgress])
-  // const [left, setLeft] = useState(0);
-  // const [right, setRight] = useState(0);
-
-  // const handleLeft = () => {
-  //   setLeft(left + 1);
-  //   console.log("left: " + left);
-  // };
-  // const handleRight = () => {
-  //   setRight(right + 1);
-  //   console.log("right: " + right);
-  // };
+  );
 
   const handleLeftImage = () => {
     if (sliderData > 0) {
@@ -62,32 +48,41 @@ const ImageSwipe = ({ images, onLoad, imageStyle, imageDescriptions }) => {
 
   useEffect(() => {
     sliderRef.current.scrollLeft = 0;
-  }, [])
-
-  // useEffect(() => {
-  //   if (imageStyle.visibility == "visible") {
-  //     sliderRef.current.scrollLeft -= 150;
-  //     console.log("scrolling left: " + left);
-  //   }
-  // }, [imageStyle, left]);
-
-  // useEffect(() => {
-  //   if (imageStyle.visibility == "visible") {
-  //     sliderRef.current.scrollLeft += 150;
-  //     console.log("scrolling right: " + right);
-  //   }
-  // }, [imageStyle, right]);
+  }, []);
 
   useEffect(() => {
-    console.log(`sliderdata: ${sliderData}`);
-  }, [sliderData]);
+    let myImg = document.querySelector("#imgId");
+    let realWidth = myImg.naturalWidth;
+    let realHeight = myImg.naturalHeight;
+    if (realWidth/realHeight <= 3 / 4) {
+      if(matches) {
+        setCurrImgContStyle({ width: "14rem", height: "24" });
+      }
+      else {
+        setCurrImgContStyle({ width: "14rem" });
+      }
+      console.log("14rem")
+    } else {
+      console.log("45rem")
+      if(matches) {
+        setCurrImgContStyle({ width: "14rem", height: "10rem" });
+      }
+      else {
+        setCurrImgContStyle({ width: "45rem" });
+      }
+    }
+  }, [currImage]);
+
+  // useEffect(() => {
+  //   console.log(`sliderdata: ${sliderData}`);
+  // }, [images, sliderData]);
 
   return (
     <div className="project-images-container" style={imageStyle}>
       {/* <p>Image swipe</p> */}
       <div className="carousel">
         <div onClick={handleLeftImage}>
-        {/* <div> */}
+          {/* <div> */}
           <LeftArrow
             isVisible={
               sliderData != 0 && imageStyle.visibility == "visible"
@@ -96,14 +91,19 @@ const ImageSwipe = ({ images, onLoad, imageStyle, imageDescriptions }) => {
             }
           />
         </div>
-        <div className="current-image-container">
+        <div className="current-image-container" style={currImgContStyle}>
           <a href={images[sliderData]} target="_blank" rel="noreferrer">
-            <img src={images[sliderData]} alt="placeholder alt text" />
+            <img
+              id="imgId"
+              src={images[sliderData]}
+              alt="placeholder alt text"
+              onLoad={() => setCurrImage(images[sliderData])}
+            />
           </a>
           <p> {imageDescriptions[sliderData]}</p>
         </div>
         <div onClick={handleRightImage}>
-        {/* <div> */}
+          {/* <div> */}
           <RightArrow
             isVisible={
               sliderData != images.length - 1 &&
@@ -131,19 +131,48 @@ const ImageSwipe = ({ images, onLoad, imageStyle, imageDescriptions }) => {
                 }
               >
                 <img src={image} onLoad={onLoad} alt="placeholder alt text" />
-                {/* <p> {image_description/label}</p> */}
               </div>
             );
           })}
         </div>
       </div>
       <div className="x-scroll-progress">
-          <div className={currentPrecent <= 20 && currentPrecent >! 20? "progress-range one active":"progress-range one"}></div>
-          <div className={currentPrecent <= 40 && currentPrecent > 20? "progress-range two active":"progress-range two"}></div>
-          <div className={currentPrecent <= 60 && currentPrecent > 40? "progress-range three active":"progress-range three"}></div>
-          <div className={currentPrecent <= 80 && currentPrecent > 60?  "progress-range four active":"progress-range four"}></div>
-          <div className={currentPrecent <= 100 && currentPrecent > 80? "progress-range five active":"progress-range five"}></div>
-        </div>
+        <div
+          className={
+            currentPrecent <= 20 && currentPrecent > !20
+              ? "progress-range one active"
+              : "progress-range one"
+          }
+        ></div>
+        <div
+          className={
+            currentPrecent <= 40 && currentPrecent > 20
+              ? "progress-range two active"
+              : "progress-range two"
+          }
+        ></div>
+        <div
+          className={
+            currentPrecent <= 60 && currentPrecent > 40
+              ? "progress-range three active"
+              : "progress-range three"
+          }
+        ></div>
+        <div
+          className={
+            currentPrecent <= 80 && currentPrecent > 60
+              ? "progress-range four active"
+              : "progress-range four"
+          }
+        ></div>
+        <div
+          className={
+            currentPrecent <= 100 && currentPrecent > 80
+              ? "progress-range five active"
+              : "progress-range five"
+          }
+        ></div>
+      </div>
     </div>
   );
 };
