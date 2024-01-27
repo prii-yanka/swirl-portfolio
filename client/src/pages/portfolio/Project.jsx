9f140ec3e9f249bba1b5114acfe0941b64157790
Priyanka Jain <jain.prii.yanka@gmail.com>
2024-01-27 02:43:34 -0500

    reusable projectDescriptionItem component + link, date and video formatting

--- client/src/pages/portfolio/Project.jsx
+++ client/src/pages/portfolio/Project.jsx
@@ -8,66 +8,51 @@ import LoadingComponent from "../../components/LoadingComponent";
 import ImageSwipe from "./ImageSwipe";
 // import Divider from '@mui/material/Divider';
 import Timeline from "@mui/lab/Timeline";
-import TimelineItem, { timelineItemClasses } from "@mui/lab/TimelineItem";
-import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import TimelineConnector from "@mui/lab/TimelineConnector";
-import TimelineContent from "@mui/lab/TimelineContent";
-import TimelineOppositeContent, {
-  timelineOppositeContentClasses,
-} from "@mui/lab/TimelineOppositeContent";
-import TimelineDot from "@mui/lab/TimelineDot";
-import Typography from "@mui/material/Typography";
+import { timelineItemClasses } from "@mui/lab/TimelineItem";
+import { timelineOppositeContentClasses } from "@mui/lab/TimelineOppositeContent";
 import HailIcon from "@mui/icons-material/Hail";
-import AdsClickIcon from '@mui/icons-material/AdsClick';
-import PsychologyAltIcon from '@mui/icons-material/PsychologyAlt';
-import OutputIcon from '@mui/icons-material/Output';
-import GroupsIcon from '@mui/icons-material/Groups';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import AdsClickIcon from "@mui/icons-material/AdsClick";
+import PsychologyAltIcon from "@mui/icons-material/PsychologyAlt";
+import OutputIcon from "@mui/icons-material/Output";
+import GroupsIcon from "@mui/icons-material/Groups";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import ProjectDescriptionItem from "./ProjectDescriptionItem";
 
-
-const labelStyle = {
-  color: "#EF8181",
-  fontWeight: "700",
-  padding: "0.5rem",
-  fontSize: "1.5rem",
-  fontFamily: "Inconsolata, monospace"
-};
-
-const contentStyle = {
-  borderRadius: "0.5rem",
-  padding: "0.5rem",
-  border: "1px solid #EF8181",
-};
-
-const iconStyle = {
-  color: "#EF8181",
-  backgroundColor: "#8FE1F3",
-  fontSize: "2rem",
-};
-
-const dotStyle = {
-  backgroundColor: "#8FE1F3",
-  justifyItems: 'flex-start'
-};
-
-const connectorStyle = {
-  bgcolor: "#ffffff",
-};
+const descList = [
+  {
+    label: "aboutTheClient",
+    icon: HailIcon,
+  },
+  {
+    label: "goalAndSituation",
+    icon: AdsClickIcon,
+  },
+  {
+    label: "processAndWhy",
+    icon: PsychologyAltIcon,
+  },
+  {
+    label: "theOutcome",
+    icon: OutputIcon,
+  },
+  {
+    label: "theTeam",
+    icon: GroupsIcon,
+  },
+];
 
 const webTimelineStyle = {
   [`& .${timelineOppositeContentClasses.root}`]: {
     flex: 0.3,
   },
-}
+};
 
 const mobileTimelineStyle = {
   [`& .${timelineItemClasses.root}:before`]: {
     flex: 0,
     padding: 0,
   },
-}
-
-
+};
 
 const Project = ({ project, selected, openModal, closeModal }) => {
   // const [id, setId] = useState();
@@ -79,7 +64,7 @@ const Project = ({ project, selected, openModal, closeModal }) => {
     // border: '1px solid black'
     visibility: "hidden",
   });
-  const matches = useMediaQuery('(max-aspect-ratio : 3/4)')
+  const matches = useMediaQuery("(max-aspect-ratio : 3/4)");
 
   // let imageStyle = {
   //   // border: '1px solid black'
@@ -203,148 +188,37 @@ const Project = ({ project, selected, openModal, closeModal }) => {
           ))}
         </div>
         <div className="description">
-          <Timeline
-            sx={!matches ? webTimelineStyle : mobileTimelineStyle}
-          >
-            <TimelineItem>
-              {!matches && <TimelineOppositeContent sx={{ m: "auto 0" }} align="right">
-                <Typography sx={labelStyle}> About The Client </Typography>
-              </TimelineOppositeContent>}
-              <TimelineSeparator>
-                <TimelineConnector sx={connectorStyle} />
-                <TimelineDot sx={dotStyle}>
-                  <HailIcon sx={iconStyle} />
-                </TimelineDot>
-                <TimelineConnector sx={connectorStyle} />
-              </TimelineSeparator>
-              <TimelineContent sx={{ py: "12px", px: 2 }}>
-                {matches && <Typography sx={labelStyle}> About The Client </Typography>}
-                <Typography sx={contentStyle}>
-                  <ul>
-                  {project.description.aboutTheClient.map((data) => (
-                    <li> {data} </li>
-                  ))}
-                  </ul>
-                </Typography>
-              </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-              {!matches && <TimelineOppositeContent sx={{ m: "auto 0" }} align="right">
-                <Typography sx={labelStyle}> Goal And Situation </Typography>
-              </TimelineOppositeContent>}
-              <TimelineSeparator>
-                <TimelineConnector sx={connectorStyle} />
-                <TimelineDot sx={dotStyle}>
-                  <AdsClickIcon sx={iconStyle} />
-                </TimelineDot>
-                <TimelineConnector sx={connectorStyle} />
-              </TimelineSeparator>
-              <TimelineContent sx={{ py: "12px", px: 2 }}>
-                {matches && <Typography sx={labelStyle}> Goal And Situation </Typography>}
-                <Typography sx={contentStyle}>
-                  <ul>
-                  {project.description.goalAndSituation.map((data) => (
-                    <li> {data} </li>
-                  ))}
-                  </ul>
-                </Typography>
-              </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-              {!matches && <TimelineOppositeContent sx={{ m: "auto 0" }} align="right">
-                <Typography sx={labelStyle}> Process And Why </Typography>
-              </TimelineOppositeContent>}
-              <TimelineSeparator>
-                <TimelineConnector sx={connectorStyle} />
-                <TimelineDot sx={dotStyle}>
-                  <PsychologyAltIcon sx={iconStyle} />
-                </TimelineDot>
-                <TimelineConnector sx={connectorStyle} />
-              </TimelineSeparator>
-              <TimelineContent sx={{ py: "12px", px: 2 }}>
-                {matches && <Typography sx={labelStyle}> Process And Why </Typography>}
-                <Typography sx={contentStyle}>
-                  <ul>
-                  {project.description.processAndWhy.map((data) => (
-                    <li> {data} </li>
-                  ))}
-                  </ul>
-                </Typography>
-              </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-              {!matches && <TimelineOppositeContent sx={{ m: "auto 0" }} align="right">
-                <Typography sx={labelStyle}> The Outcome </Typography>
-              </TimelineOppositeContent>}
-              <TimelineSeparator>
-                <TimelineConnector sx={connectorStyle} />
-                <TimelineDot sx={dotStyle}>
-                  <OutputIcon sx={iconStyle} />
-                </TimelineDot>
-                <TimelineConnector sx={connectorStyle} />
-              </TimelineSeparator>
-              <TimelineContent sx={{ py: "12px", px: 2 }}>
-                {matches && <Typography sx={labelStyle}> The Outcome </Typography>}
-                <Typography sx={contentStyle}>
-                  <ul>
-                  {project.description.theOutcome.map((data) => (
-                    <li> {data} </li>
-                  ))}
-                  </ul>
-                </Typography>
-              </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-              {!matches && <TimelineOppositeContent sx={{ m: "auto 0" }} align="right">
-                <Typography sx={labelStyle}> The Team </Typography>
-              </TimelineOppositeContent>}
-              <TimelineSeparator>
-                <TimelineConnector sx={connectorStyle} />
-                <TimelineDot sx={dotStyle}>
-                  <GroupsIcon sx={iconStyle} />
-                </TimelineDot>
-                <TimelineConnector sx={connectorStyle} />
-              </TimelineSeparator>
-              <TimelineContent sx={{ py: "12px", px: 2 }}>
-                {matches && <Typography sx={labelStyle}> The Team </Typography>}
-                <Typography sx={contentStyle}>
-                  <ul>
-                  {project.description.theTeam.map((data) => (
-                    <li> {data} </li>
-                  ))}
-                  </ul>
-                </Typography>
-              </TimelineContent>
-            </TimelineItem>
+          <Timeline sx={!matches ? webTimelineStyle : mobileTimelineStyle}>
+            {descList.map((descItem, i) => (
+              <ProjectDescriptionItem
+                content={project.description[descItem.label]}
+                label={Object.keys(project.description)[i]}
+                icon={descItem.icon}
+                matches={matches}
+              />
+            ))}
           </Timeline>
         </div>
         {isVideo && (
-          <>
-            <div className="video-container">
-              <div className="label">
-                <div>
-                  Video: <br /> <br />
-                </div>
-              </div>
-              <video src={project.video} controls>
-                {/* <source  type="video/mp4" /> */}
-              </video>
-              <br /> <br />
-            </div>
-          </>
+          <div className="video-container">
+            <div className="label">Demo Video:</div>
+            <video src={project.video} controls></video>
+          </div>
         )}
-        <div>
+        <div className="projectDate">
           {" "}
           {moment(project.when[0]).format("MMMM Do, YYYY")} -{" "}
           {moment(project.when[1]).format("MMMM Do, YYYY")} <br /> <br />{" "}
         </div>
-        <div>
-          {" "}
-          <a href={project.linkTo} target="_blank" rel="noreferrer">
+        {project.linkTo && (
+          <div className="projectLink">
             {" "}
-            {project.linkTo}{" "}
-          </a>{" "}
-        </div>
+            <a href={project.linkTo} target="_blank" rel="noreferrer">
+              {" "}
+              {project.linkTo}{" "}
+            </a>{" "}
+          </div>
+        )}
       </div>
     </Modal>
   );
