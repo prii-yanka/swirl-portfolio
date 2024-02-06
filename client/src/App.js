e6c51aee8a42645773babb692eef72e75c4001d4
Priyanka Jain <jain.prii.yanka@gmail.com>
2024-02-06 08:08:14 -0500

    mobile loading error fix attempt

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,18 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     setLoading(true);
-    window.addEventListener(
-      "load",
-      function () {
-        // loaded
-        setLoading(false);
-      },
-      false
-    );
+    // window.addEventListener(
+    //   "load",
+    //   function () {
+    //     // loaded
+    //     setLoading(false);
+    //   },
+    //   false
+    // );
+
+    window.onload = (event) => {
+      setLoading(false);
+    };
   }, []);
 
   return (
