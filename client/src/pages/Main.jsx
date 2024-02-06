ea0371436b8a53e9049e5eb56f69ba17fad8e7c1
Priyanka Jain <jain.prii.yanka@gmail.com>
2024-02-06 09:17:47 -0500

    change from browser router to routerprovider

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,10 +1,14 @@
-import React, { useState, useEffect, useLayoutEffect, useRef } from 'react';
-import './main.css';
-import { Contact, Education, Experience, Intro, Portfolio, Skills } from './';
+import React, { useState, useEffect, useLayoutEffect, useRef } from "react";
+import "./main.css";
+import { Contact, Education, Experience, Intro, Portfolio, Skills } from "./";
 // import { useSpring, animated } from 'react-spring'
 // import styled, { css } from 'styled-components'
 import { ScrollAnimation } from "./ScrollAnimation";
 import { BrowserRouter } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 const Main = (props) => {
   const mainContainerRef = useRef(null);
@@ -14,45 +18,58 @@ const Main = (props) => {
   const handleResize = () => {
     let highestSlide = 0;
     let widestSlide = 0;
-    if (mainContainerRef.current && highestSlide < mainContainerRef.current.offsetHeight) {
+    if (
+      mainContainerRef.current &&
+      highestSlide < mainContainerRef.current.offsetHeight
+    ) {
       highestSlide = mainContainerRef.current.offsetHeight;
     }
-    if (mainContainerRef.current && widestSlide < mainContainerRef.current.offsetWidth) {
+    if (
+      mainContainerRef.current &&
+      widestSlide < mainContainerRef.current.offsetWidth
+    ) {
       widestSlide = mainContainerRef.current.offsetWidth;
     }
     setHeight(highestSlide);
     setWidth(widestSlide);
-  }
+  };
 
   useEffect(() => setHeight(mainContainerRef.current.offsetHeight), []);
   useEffect(() => setWidth(mainContainerRef.current.offsetWidth), []);
 
   useEffect(() => {
-    console.log(`height: ${height}`)
-    console.log(`width: ${width}`)
-  }, [height, width])
+    console.log(`height: ${height}`);
+    console.log(`width: ${width}`);
+  }, [height, width]);
 
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-        window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
-}, []);
+  }, []);
+
+  const router = createBrowserRouter([
+    { path: "*", Component: Portfolio },
+  ]);
 
   return (
     <div className="main-container">
-      <ScrollAnimation mainContainerHeight={height || "5400"} mainContainerWidth={width || "1920"}/>
-      <main className='parts' ref={mainContainerRef}>
+      <ScrollAnimation
+        mainContainerHeight={height || "5400"}
+        mainContainerWidth={width || "1920"}
+      />
+      <main className="parts" ref={mainContainerRef}>
         <Intro />
         <Education />
         <Skills />
         {/* <Routes>
             <Route exact path="/" element={<Portfolio/>} />
         </Routes> */}
-        <BrowserRouter>
+        {/* <BrowserRouter>
           <Portfolio />
-          {/* <Navigate to="/"/> */}
-        </BrowserRouter>
+        </BrowserRouter> */}
+        <RouterProvider router={router}/>
         <Contact />
       </main>
     </div>
@@ -60,4 +77,3 @@ const Main = (props) => {
 };
 
 export default Main;
-
